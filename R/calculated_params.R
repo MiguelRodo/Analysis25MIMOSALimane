0acb603004c534a9115e2a82207887b18f8f1a04
Siphosethu Limane <LMNSIP001@myuct.ac.za>
2025-08-27 11:39:22 +0200

    Update calculated_params.R

--- R/calculated_params.R
+++ R/calculated_params.R
@@ -1,29 +1,24 @@
 
-calculated_params <- function(mu_u_NR, mu_u_R, f, c_R, c_NR){
-  mu_s <- mu_u_NR * (1 + f)# Stimulated mean (fixed for all) used f = 1
-  # Convert means for beta distribution
-  a_s_R  <- c_R  * mu_s
-  b_s_R  <- c_R  * (1 - mu_s)
+calculated_params <- function(mu_u_NR, mu_u_R, f, c_u, c_s) {
+  
+  # Stimulated condition 
+  mu_s <- mu_u_R * (1 + f)  
+  a_s <- c_s * mu_s
+  b_s <- c_s * (1 - mu_s)
+  
+  # Unstimulated responders 
+  a_u_R <- c_u * mu_u_R
+  b_u_R <- c_u * (1 - mu_u_R)
+  
+  # Unstimulated non-responders 
+  a_u_NR <- c_u * mu_u_NR
+  b_u_NR <- c_u * (1 - mu_u_NR)
 
-  a_s_NR <- c_NR * mu_s
-  b_s_NR <- c_NR * (1 - mu_s)
-
-  # Unstimulated
-  a_u_R  <- c_R  * mu_u_R
-  b_u_R  <- c_R  * (1 - mu_u_R)
-
-  a_u_NR <- c_NR * mu_u_NR
-  b_u_NR <- c_NR * (1 - mu_u_NR)
-
-  # return as a named list
   return(list(
-    a_s_R    = a_s_R,
-    b_s_R    = b_s_R,
-    a_s_NR   = a_s_NR,
-    b_s_NR = b_s_NR,
-    a_u_R  = a_u_R,
-    b_u_R  = b_u_R,
-    a_u_NR = a_u_NR,
-    b_u_NR = b_u_NR
+    a_s = a_s, b_s = b_s,
+    a_u_R = a_u_R, b_u_R = b_u_R,
+    a_u_NR = a_u_NR, b_u_NR = b_u_NR,
+    mu_s = mu_s, mu_u_R = mu_u_R, mu_u_NR = mu_u_NR
   ))
 }
+
