839e40bcc8f37841511d38d0123efa22f157b262
Miguel Rodo <miguel.rodo@uct.ac.za>
2025-10-21 14:27:02 +0000

    Correct noise simulations

--- R/fit_MIMOSA.R
+++ R/fit_MIMOSA.R
@@ -1,10 +1,11 @@
-fit_MIMOSA <- function(data) {
+fit_MIMOSA <- function(data, mtd = "mcmc") {
   sink("output.txt", append = TRUE)
   fit <- MIMOSA(
     NSUB + CYTNUM ~ SUBJECTID | CYTOKINE,
     data = data,
     subset = RefTreat == "Treatment" & CYTOKINE == "IL2",
     ref = RefTreat == "Reference" & CYTOKINE == "IL2",
+    method = mtd,
     RT = FALSE
   )
   sink(NULL)
